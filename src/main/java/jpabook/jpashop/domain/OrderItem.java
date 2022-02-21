ef47e5854048b5a39ca5088c611631f4586439de
yooja <wogud758787@naver.com>
2022-02-21 19:33:17 +0900

    entity

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private int orderPrice;
+    private int count;
+}
